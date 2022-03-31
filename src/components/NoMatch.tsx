19e82a5a07c90bf46ed1d5ac9ea95d4b524c378f
kay <kay.vieider@hgv.it>
2022-03-31 22:24:04 +0200

    Add fail page

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from "react";
+import styles from "./NoMatch.module.css";
 
-const NoMatch: FC = () => {
-  return <>no Match</>;
-};
+const NoMatch: FC = () => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>Sorry, something failed!</h1>
+    <p className={styles.text}>It looks like you have called a dead link.</p>
+  </div>
+);
 
 export default NoMatch;
